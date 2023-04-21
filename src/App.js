d2c8221b9ce1f2320f48a0ba520719a626c3d227
Sarans13 <114057996+Sarans13@users.noreply.github.com>
2023-04-21 11:52:06 +0530

    Fixed Navbar - Issue 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+// import Signup from "./Signup"
+// import Profile from "./Profile"
+import Navbar from "./Navbar"
+// import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      {/* <Router>
+        <Routes>
+          <Route exact path="/" element={<Signup/>} />
+          <Route exact path="/profile" element={<Profile/>} />
+        </Routes>
+      </Router> */}
     </div>
   );
 }
