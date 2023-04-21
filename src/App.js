47134f170d81ec0ca94c2d305567328d7dbb7fc9
Sarans13 <114057996+Sarans13@users.noreply.github.com>
2023-04-21 12:36:30 +0530

    Fixed React Props - Issue 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import './App.css';
-// import Signup from "./Signup"
-// import Profile from "./Profile"
+import React, { useState } from 'react';
+import SignupForm from './Signup';
+import ProfilePage from './Profile';
 import Navbar from "./Navbar"
-// import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
 
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  const handleSignup = (user) => {
+    setUsers([...users, user]);
+    // Store the user data locally
+    localStorage.setItem('users', JSON.stringify(users));
+  };
+
   return (
-    <div className="App">
+    <div>
       <Navbar/>
-      {/* <Router>
-        <Routes>
-          <Route exact path="/" element={<Signup/>} />
-          <Route exact path="/profile" element={<Profile/>} />
-        </Routes>
-      </Router> */}
+      {users.length === 0 && <SignupForm onSubmit={handleSignup} />}
+      {users.length > 0 && <ProfilePage users={users} />}
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import Signup from "./Signup"
+// import Profile from "./Profile"
+// import Navbar from "./Navbar"
+// import { BrowserRouter as Router, Route , Routes} from "react-router-dom";
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar/>
+//       <Router>
+//         <Routes>
+//           <Route exact path="/" element={<Signup/>} />
+//           <Route exact path="/profile" element={<Profile/>} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
