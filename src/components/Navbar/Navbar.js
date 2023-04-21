717c7608e8d79738eee54ae04214934e071cd541
sahildkun <sahilsahoo23@gmail.com>
2023-04-21 11:17:39 +0530

    account logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,24 @@
 import React from 'react'
-
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 const Navbar = () => {
   return (
     <div className=' p-5 shadow-2xl'>
        <ul className='flex flex-row justify-between items-center '>
          <li className='text-3xl font-bold'>
-           OPEN THE SOURCE
+         <a href='/'>OPEN THE SOURCE</a>  
          </li>
-         <div className='flex flex-row space-x-10 '>
+         <div className='flex flex-row space-x-10 items-center '>
+        <li>
+          <a href="/">REACT</a> 
+         </li>
          <li>
-            HOME
-         </li><li>
-           REACT
-         </li><li>
-            FLUTTER
-         </li><li>
-            BACKEND
+         <a href="/">FLUTTER</a> 
+         </li>
+         <li>
+         <a href="/">NODE JS</a> 
+         </li>
+         <li>
+            <AccountCircleIcon fontSize='large'/>
          </li>
          </div>
        </ul>
